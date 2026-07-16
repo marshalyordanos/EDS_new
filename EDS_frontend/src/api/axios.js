72679195ba9143faa22b5b4b1d0c94e3ccdfea43
marshalyordanos <marshalyordanosseifu@gmail.com>
2026-07-16 03:26:53 +0300

    changes

--- EDS_frontend/src/api/axios.js
+++ EDS_frontend/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { refreshToken } from "../services/authService";
 
 // Get API base URL from environment variables
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL =  "http://159.223.188.205:8000";
 
 console.log("API Base URL:", API_BASE_URL);
 
